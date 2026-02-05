f6d5a952f8419aca06c181b953faa431f970fde8
teshan200 <git.teshan@gmail.com>
2026-02-05 19:04:11 +0530

    .env example

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3001/api';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001/api';
 
 // Create axios instance
 const api = axios.create({
